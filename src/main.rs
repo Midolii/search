988edb33fbff04376bda7a3cf62fd9066d91a1aa
Midolii <462885536@qq.com>
2024-04-12 22:44:19 +0800

    feat: 添加默认值

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, time::Instant};
 
-use crate::search::{search_key, SearchResult};
+use crate::search::{search_key, Profiles, SearchResult};
 
 #[path = "./tools/file_tools.rs"]
 mod file_tools;
@@ -9,9 +9,16 @@ mod search;
 
 fn run_search() -> Instant {
     let now = Instant::now();
-    let profiles = search::get_profiles().unwrap();
+    let profiles = search::get_profiles().unwrap_or(Profiles {
+        key: None,
+        entry: Some("./".to_owned()),
+        output: None,
+        include_file: Some(vec![]),
+        exclude_file: Some(vec![]),
+        exclude_extension: Some(vec![]),
+    });
     let file_list = search::get_path_list(
-        Path::new(&profiles.entry),
+        Some(Path::new(&profiles.entry.unwrap_or("./".to_owned()))),
         &profiles.include_file,
         &profiles.exclude_file,
         &profiles.exclude_extension,
@@ -21,9 +28,11 @@ fn run_search() -> Instant {
     let mut searched_lines: usize = 0;
     let mut founded_key_file_num: usize = 0;
     for file_path in &file_list {
-        // 排除output文件
-        if file_path.contains(&profiles.output) {
-            continue;
+        if let Some(output) = &profiles.output {
+            // 排除output文件
+            if file_path.contains(output) {
+                continue;
+            }
         }
         let mut result = search_key(&Path::new(file_path), &profiles.key);
         searched_lines += result.total_lines;
@@ -33,8 +42,14 @@ fn run_search() -> Instant {
         }
     }
 
-    // file_tools::write_json_to(&Path::new(&profiles.output), &file_list);
-    file_tools::write_json_to(&Path::new(&profiles.output), &result_object);
+    match profiles.output {
+        Some(output) => {
+            file_tools::write_json_to(&Path::new(&output), &result_object);
+        }
+        None => {
+            println!("{:#?}", serde_json::to_string(&result_object));
+        }
+    }
 
     println!(
         "本次搜索 {:#?} 个文件，{:#?} 行, 在 {:#?} 个文件中发现结果",
--- src/tools/search.rs
+++ src/tools/search.rs
@@ -8,9 +8,9 @@ use std::{
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Profiles {
-    pub key: String,
-    pub entry: String,
-    pub output: String,
+    pub key: Option<String>,
+    pub entry: Option<String>,
+    pub output: Option<String>,
     pub include_file: Option<Vec<String>>,
     pub exclude_file: Option<Vec<String>>,
     pub exclude_extension: Option<Vec<String>>,
@@ -23,8 +23,17 @@ pub fn get_profiles() -> Option<Profiles> {
     let args = env::args();
     match args.len() {
         1 => {
+            let mut _read_path: &Path = Path::new("./");
+            match Path::new(DEFAULT_APPLICATION_YML).exists() {
+                true => {
+                    _read_path = Path::new(DEFAULT_APPLICATION_YML);
+                }
+                false => {
+                    return None;
+                }
+            }
             let profiles: Profiles = serde_yaml::from_str(
-                fs::read_to_string(Path::new(DEFAULT_APPLICATION_YML))
+                fs::read_to_string(_read_path)
                     .expect("没有找到配置文件，请前往 https://github.com/Midolii/search 查看文档")
                     .as_str(),
             )
@@ -37,44 +46,56 @@ pub fn get_profiles() -> Option<Profiles> {
 
 /// 获取path下所有文件的名称
 pub fn get_path_list(
-    entry: &Path,
+    entry: Option<&Path>,
     include_file: &Option<Vec<String>>,
     exclude_file: &Option<Vec<String>>,
     exclude_extension: &Option<Vec<String>>,
 ) -> Vec<String> {
     let mut result: Vec<String> = vec![];
-    // 如果是链接文件则略过
-    if entry.is_symlink() {
-        return result;
-    }
+    match entry {
+        Some(entry) => {
+            // 如果是链接文件则略过
+            if entry.is_symlink() {
+                return result;
+            }
 
-    if let Some(exclude) = exclude_file {
-        for key in exclude {
-            if entry.to_str().unwrap().contains(key) {
-                return result;
+            if let Some(exclude) = exclude_file {
+                for key in exclude {
+                    if entry.to_str().unwrap().contains(key) {
+                        return result;
+                    }
+                }
+            }
+            if entry.is_file() {
+                if let Some(exclude) = exclude_extension {
+                    match &entry.extension() {
+                        Some(extension) => {
+                            if exclude.contains(&extension.to_str().unwrap().to_owned()) {
+                                return result;
+                            }
+                        }
+                        None => {
+                            return result;
+                        }
+                    }
+                }
+                result.push(entry.to_str().unwrap().to_owned());
+            }
+            if entry.is_dir() {
+                let files = read_dir(entry).unwrap();
+                for file_name in files {
+                    if let Ok(file) = file_name {
+                        result.append(&mut get_path_list(
+                            Some(&file.path()),
+                            include_file,
+                            exclude_file,
+                            exclude_extension,
+                        ));
+                    }
+                }
             }
         }
-    }
-    if entry.is_file() {
-        if let Some(exclude) = exclude_extension {
-            if exclude.contains(&entry.extension().unwrap().to_str().unwrap().to_owned()) {
-                return result;
-            }
-        }
-        result.push(entry.to_str().unwrap().to_owned());
-    }
-    if entry.is_dir() {
-        let files = read_dir(entry).unwrap();
-        for file_name in files {
-            if let Ok(file) = file_name {
-                result.append(&mut get_path_list(
-                    &file.path(),
-                    include_file,
-                    exclude_file,
-                    exclude_extension,
-                ));
-            }
-        }
+        None => {}
     }
     result
 }
@@ -92,28 +113,30 @@ pub struct SearchResultWithLines {
 }
 
 /// 根据传入的文件路径和关键字在文件中进行查找对应行，最终返回所有匹配到的行以及文件行数，如果没有匹配到的结果则也会返回文件总行数
-pub fn search_key(target: &Path, key: &String) -> SearchResultWithLines {
+pub fn search_key(target: &Path, key: &Option<String>) -> SearchResultWithLines {
     let mut result: Vec<SearchResult> = vec![];
     let mut line_num = 0;
-    match read(target) {
-        Ok(reader) => {
-            reader.lines().for_each(|line| match line {
-                Ok(line) => {
-                    line_num += 1;
-                    if line.contains(key) {
-                        result.push(SearchResult {
-                            path: target.to_str().unwrap().to_owned(),
-                            line: line_num,
-                            content: line.trim().to_owned(),
-                        })
+    match key {
+        Some(key) => match read(target) {
+            Ok(reader) => {
+                reader.lines().for_each(|line| match line {
+                    Ok(line) => {
+                        line_num += 1;
+                        if line.contains(key) {
+                            result.push(SearchResult {
+                                path: target.to_str().unwrap().to_owned(),
+                                line: line_num,
+                                content: line.trim().to_owned(),
+                            })
+                        }
                     }
-                }
-                Err(_) => {}
-            });
-        }
-        Err(_) => {}
+                    Err(_) => {}
+                });
+            }
+            Err(_) => {}
+        },
+        None => {}
     }
-
     SearchResultWithLines {
         search_result_list: result,
         total_lines: line_num,
